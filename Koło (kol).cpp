f49ae062957b22966411985039c8b62c9f5b73be
Lukasz Jakubowski <56789574+woocashh@users.noreply.github.com>
2019-10-30 19:29:19 +0000

    Create  Koło (kol).cpp

---
+++ Koło (kol).cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+
+
+
+
+
+int main() {
+    double r;
+    
+    cin>>r;
+    
+    double pi = M_PI;
+    
+    
+    cout<< setprecision(3)<<fixed;
+    
+    cout<<pi*r*r<<endl;
+    cout<<2*pi*r;
+
+
+    return 0;
+}
