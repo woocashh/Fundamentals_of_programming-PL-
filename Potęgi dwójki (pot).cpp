238c6aaaaa802aa17ec58aad18b2c4642b88194a
Lukasz Jakubowski <56789574+woocashh@users.noreply.github.com>
2019-10-30 19:42:10 +0000

    Create Potęgi dwójki (pot).cpp

---
+++ Potęgi dwójki (pot).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+    
+    int n;
+    cin>>n;
+    
+    int x=1;
+    
+    while (x<=n){
+        
+        cout<<x<<endl;
+        x*=2;
+    }
+    
+
+
+    return 0;
+}
